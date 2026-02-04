076f8403d03c7648111e20d1ae53d4ef9b845207
Hiraina <hei.hiraina@gmail.com>
2026-02-04 21:38:06 +0300

    feat: switch theme toggle button logo for dark mode

--- js/theme.js
+++ js/theme.js
@@ -1,23 +1,27 @@
-export function switchTheme() {
-  const body = document.body;
-  const logo = document.querySelector("#logo");
-  body.classList.toggle("dark");
+const body = document.body;
+const logo = document.querySelector("#logo");
+const icon = document.querySelector(".theme-toggle-btn i");
 
-  if (body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-    logo.src = "assets/images/logo-for-dark-mode.png";
-  } else {
-    localStorage.setItem("theme", "light");
-    logo.src = "assets/images/logo.png";
-  }
+export function switchTheme() {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+        logo.src = "assets/images/logo-for-dark-mode.png";
+        icon.className = "fas fa-sun";
+    } else {
+        localStorage.setItem("theme", "light");
+        logo.src = "assets/images/logo.png";
+        icon.className = "fa-regular fa-moon";
+    }
 }
 
 export function initTheme() {
-  const body = document.body;
-  const savedTheme = localStorage.getItem("theme");
+    const savedTheme = localStorage.getItem("theme");
 
-  if (savedTheme === "dark") {
-    body.classList.add("dark");
-    logo.src = "assets/images/logo-for-dark-mode.png";
-  }
+    if (savedTheme === "dark") {
+        body.classList.add("dark");
+        logo.src = "assets/images/logo-for-dark-mode.png";
+        icon.className = "fas fa-sun";
+    }
 }
